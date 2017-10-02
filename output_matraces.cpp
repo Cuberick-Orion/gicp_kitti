37871f60f9ba3f42960b7a6a40c6c16a5a98eb84
Cuberick-Orion <u5689359@anu.edu.au>
2017-10-03 00:04:31 +1100

    Null

--- output_matraces.cpp
+++ output_matraces.cpp
@@ -10,19 +10,23 @@ int main() {
 	char frame2[100];
 	char command[300];
 
-	char *directory = "~/gicp/data/data_interval_6/frame_data_";
+	// change the directory to where you store the ascii files that produced by MATLAB
+	char *directory = "~/gicp/data/dataset_08/frame_data_";
 	char *fileType = ".ascii";
 	char *exeLocation = "~/gicp/test_gicp";
 	char *parameters = "--debug 0";
 
+	// change the for loop no. of iterations here
 // total of 105 frames, produce 104 datasets
-	for (int count = 1; count<107; count++) {
+	for (int count = 1; count<407; count++) {
 		int fileNo1 = count;
 		int fileNo2 = (count + 1);
 		sprintf(frame1,"%s%d%s",directory,fileNo1,fileType);
 		sprintf(frame2,"%s%d%s",directory,fileNo2,fileType);
 
 		// sprintf(command,"%s %s %s",exeLocation,frame2,frame1);
+
+		// NOTE: frame 2 comes first!!!
 		sprintf(command,"%s %s %s %s",exeLocation,frame2,frame1,parameters);
 		int result = system(command);
 
@@ -33,3 +37,8 @@ int main() {
 
 	return 0;
 }
+
+// Execution:
+// g++ hello.cpp
+// make
+// ./a.out &>> ~/gicp/total_output.txt
